810ea6dc6d81ff1c5c81af37a676d01dbdc21352
Jap Mul lekkereboy <mul.jasper@gmail.com>
2012-09-18 17:33:00 +0200

    first commit

---
+++ src/edu/self/ipsen6/MainActivity.java
@@ -0,0 +1,28 @@
+package edu.self.ipsen6;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        Intent registrationIntent = new Intent("com.google.android.c2dm.intent.REGISTER");
+        registrationIntent.putExtra("app", PendingIntent.getBroadcast(this, 0, new Intent(), 0)); // boilerplate
+        registrationIntent.putExtra("sender", "mul.jasper@gmail.com");
+        startService(registrationIntent);
+        
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
