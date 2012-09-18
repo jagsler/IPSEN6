1a86e5047c9abbc09a39025c195313c5404193af
Jap Mul lekkereboy <mul.jasper@gmail.com>
2012-09-18 17:53:02 +0200

    Notificatie bericht en readmessage activity toegevoegd

--- src/edu/self/ipsen6/MainActivity.java
+++ src/edu/self/ipsen6/MainActivity.java
@@ -2,12 +2,18 @@ package edu.self.ipsen6;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
+	private static final int NOTIFICATION_ID = 1;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +24,27 @@ public class MainActivity extends Activity {
         registrationIntent.putExtra("sender", "mul.jasper@gmail.com");
         startService(registrationIntent);
         
+    }
+    
+    public void test(View v) {
+    	String ns = Context.NOTIFICATION_SERVICE;
+    	NotificationManager mNotificationManager = (NotificationManager) getSystemService(ns);
+    	
+    	int icon = R.drawable.ic_launcher;
+    	CharSequence tickerText = "Tickertext";
+    	long when = System.currentTimeMillis();
+
+    	Notification notification = new Notification(icon, tickerText, when);
+    	
+    	Context context = getApplicationContext();
+    	CharSequence contentTitle = "Title";
+    	CharSequence contentText = "Hier dan het begin van een hele lap tekst ofzo";
+    	Intent notificationIntent = new Intent(this, ReadMessage.class);
+    	PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+    	notification.setLatestEventInfo(context, contentTitle, contentText, contentIntent);
+
+    	mNotificationManager.notify(NOTIFICATION_ID, notification);
     }
 
     @Override
---
+++ src/edu/self/ipsen6/ReadMessage.java
@@ -0,0 +1,15 @@
+package edu.self.ipsen6;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ReadMessage extends Activity {
+
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.read_message);  
+    }
+
+}
