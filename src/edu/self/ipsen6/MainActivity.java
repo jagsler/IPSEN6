a6fd11172bfc4fdd00db74580d10f6a0afad1124
Dennis Wethmar <denniswethmar@gmail.com>
2012-09-25 12:21:15 +0200

    ik heb gewerkt aan de lijst

--- src/edu/self/ipsen6/MainActivity.java
+++ src/edu/self/ipsen6/MainActivity.java
@@ -1,55 +1,35 @@
 package edu.self.ipsen6;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
+import android.app.ListActivity;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity {
 
 	private static final int NOTIFICATION_ID = 1;
 	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        Intent registrationIntent = new Intent("com.google.android.c2dm.intent.REGISTER");
-        registrationIntent.putExtra("app", PendingIntent.getBroadcast(this, 0, new Intent(), 0)); // boilerplate
-        registrationIntent.putExtra("sender", "mul.jasper@gmail.com");
-        startService(registrationIntent);
-        
-    }
-    
-    public void test(View v) {
-    	String ns = Context.NOTIFICATION_SERVICE;
-    	NotificationManager mNotificationManager = (NotificationManager) getSystemService(ns);
-    	
-    	int icon = R.drawable.ic_launcher;
-    	CharSequence tickerText = "Tickertext";
-    	long when = System.currentTimeMillis();
+	static final String[] MOBILE_OS = 
+            new String[] { "Android", "iOS", "WindowsMobile", "Blackberry"};
+		
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setListAdapter(new MessageArrayAdapter(this, MOBILE_OS));
+	}
+ 
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		//get selected items
+		String selectedValue = (String) getListAdapter().getItem(position);
+		Toast.makeText(this, selectedValue, Toast.LENGTH_SHORT).show();
+	}
 
-    	Notification notification = new Notification(icon, tickerText, when);
-    	
-    	Context context = getApplicationContext();
-    	CharSequence contentTitle = "Title";
-    	CharSequence contentText = "Hier dan het begin van een hele lap tekst ofzo";
-    	Intent notificationIntent = new Intent(this, ReadMessage.class);
-    	PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
-
-    	notification.setLatestEventInfo(context, contentTitle, contentText, contentIntent);
-
-    	mNotificationManager.notify(NOTIFICATION_ID, notification);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
 }
